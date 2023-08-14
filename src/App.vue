88b1a56c99206a918617104a92a96e69f31433a5
web.artur.work <65858592+WebArturWork@users.noreply.github.com>
2023-08-14 10:54:18 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, idx) in items" :key="idx">
+        {{ item.name }} - {{ item.price }}
+      </li>
+    </ul>
+    <button @click="addAndUpdate">Add and update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [
+        { name: "Alph", price: 1 },
+        { name: "Beta", price: 2 },
+      ],
+    };
+  },
+
+  methods: {
+    addAndUpdate() {
+      const newItem = { name: "New", price: "-" };
+      this.items.push(newItem);
+      console.log(this.items);
+
+      setTimeout(() => {
+        newItem.price = "over 9000";
+      }, 3000);
+    },
+  },
+};
+</script>
